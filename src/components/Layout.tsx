419e29be50d69693d3727fa2ca95899ad0bccb19
Marc Mariné <shenobi@gmail.com>
2025-07-03 22:11:17 +0200

    Ensure page scrolls to top on navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router'
 import Header from './Header'
 
 export default function Layout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Header />
