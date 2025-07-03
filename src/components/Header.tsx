b2555ee1502b54b2afbec3479c76911173dc89bf
Marc Mariné <shenobi@gmail.com>
2025-07-03 22:43:31 +0200

    Add GitHub link to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from 'react-router'
 import logo from '../assets/logo.svg'
+import { GITHUB_REPO_URL } from '../lib/constants'
 
 export default function Header() {
   const navigate = useNavigate()
@@ -24,6 +25,13 @@ export default function Header() {
                   className="w-16 group-hover:-rotate-2 group-hover:scale-95 group-active:scale-90 transition-all drop-shadow-lg group-hover:drop-shadow-md group-active:drop-shadow-sm duration-300"
                 />
               </button>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href={GITHUB_REPO_URL} className="secondary" target="_blank">
+                About
+              </a>
             </li>
           </ul>
         </nav>
